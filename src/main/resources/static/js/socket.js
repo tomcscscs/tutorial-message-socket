feadb5799c23ae77b86c2f3cd87e6391c111067d
tomcscscs <bright.chan88@gmail.com>
2024-02-14 16:16:04 +0900

    선생님꺼 완료

--- src/main/resources/static/js/socket.js
+++ src/main/resources/static/js/socket.js
@@ -2,18 +2,27 @@
  * 
  */
 //WebSocket 연결 생성
+const human = {
+	name : "홍길동",
+	age : 17
+}
+console.log(JSON.stringify(human));
 
 const socket = new WebSocket("ws://" + location.host + "/server");
 
 // 메시지 수신
 socket.addEventListener("message", function(event) {
 	console.log(event.data);
-	switch (event.data) {
+	const obj =JSON.parse(event.data);
+	
+	switch (obj.type) {
 		case "newFeed":
-			window.alert("새 피드가 추가되었습니다.");
+			if(window.confirm(`새 피드가 추가되었습니다.\n[${obj.title}]를 바로 확인하시겠습니까?`)){
+				location.href="/feed/"+obj.feedId;				
+			}
 			break;
 		case "newNotice":
 			window.alert("새 공지가 추가되었습니다.");
 			break;
 	}
-});	
+});
